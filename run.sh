5154f8553c8aa82bb5272c965b0bb5aaf4321fa6
m31271n <m31271n@gmail.com>
2015-07-21 10:31:02 +0800

    Giving container a specific name can avoid creating spare containers that have same function

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 . ./env.cfg
 
 docker run -it --rm \
-    -v $(pwd)/data:/var/lib/clamav \
-    ${IMAGE_NAME}
+       -v $(pwd)/data:/var/lib/clamav \
+       --name ${CONTAINER_NAME} \
+       ${IMAGE_NAME}
