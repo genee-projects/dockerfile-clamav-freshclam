932114c9189c401f38e22c36e95bf6af8b8c64a9
Rui Ma <rui.ma@geneegroup.com>
2015-08-25 11:13:58 +0900

    修正 run.sh 脚本, 结果病毒库可能被错误抓取的问题

--- run.sh
+++ run.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+p=$(pwd)
+
 #解决非当前路径执行的情况
-rel_path=`dirname $0`
+rel_path=$(cd `dirname $0`; pwd)
 
-. $rel_path/env.cfg
+cd "$p"
 
+. "$rel_path/env.cfg"
+
+#先复制
+cp -r "$rel_path/data" "$rel_path/tmp"
+
+echo "===开始更新==="
+
+#再更新
 docker run -it --rm \
-       -v $rel_path/data:/var/lib/clamav \
+       -v $rel_path/tmp:/var/lib/clamav \
        --name ${CONTAINER_NAME} \
        ${IMAGE_NAME}
+
+#再替换
+rm -rf "$rel_path/data" && mv "$rel_path/tmp" "$rel_path/data"
+
+echo "===更新结束==="
