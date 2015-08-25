854d82a2e5373df5bedaa4dcde8f8e31eb6bb85b
Rui Ma <rui.ma@geneegroup.com>
2015-08-25 10:46:20 +0900

    解决非当前路径执行的情况

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-. ./env.cfg
+#解决非当前路径执行的情况
+rel_path=`dirname $0`
+
+. $rel_path/env.cfg
 
 docker run -it --rm \
-       -v $(pwd)/data:/var/lib/clamav \
+       -v $rel_path/data:/var/lib/clamav \
        --name ${CONTAINER_NAME} \
        ${IMAGE_NAME}
